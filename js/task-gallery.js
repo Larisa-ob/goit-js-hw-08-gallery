627964e72b981e5792a60c8126e334007ce53fed
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-11-19 22:48:09 +0200

    Исходніе файлі 8 задачи

---
+++ js/task-gallery.js
@@ -0,0 +1,5 @@
+const ref = {
+  buttonDecrement: document.querySelector('button[data-action="decrement"]'),
+  htmlValue: document.querySelector("#value"),
+  buttonIncrement: document.querySelector('button[data-action="increment"]'),
+};
