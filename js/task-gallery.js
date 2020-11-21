d6f5a3b44ca418fdd432baad75e92929fdabbd3a
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-11-21 23:06:02 +0200

    Подготовка к загрузке модального окна

--- js/task-gallery.js
+++ js/task-gallery.js
@@ -3,32 +3,34 @@ import galleryItems from "./gallery-items.js";
 const refs = {
   galleryItem: document.querySelector(".gallery__item"),
   gallery: document.querySelector(".js-gallery"),
-  largeImage: document.querySelector(".js-large-image"),
+  modalInput: document.querySelector(".lightbox"),
+  largeImage: document.querySelector(".lightbox-image"),
 };
+
 const array = [...galleryItems];
-console.log(array);
 const elementGalleryRef = (array) =>
   array.forEach((element) => {
     /* создаем img класса gallery__image */
     const img = document.createElement("img");
     img.classList.add("gallery__image");
-    /*img.src = element.original;*/
-    img.setAttribute("data-source", element.preview);
+    img.src = element.preview;
+    img.setAttribute("data-source", element.original);
     img.alt = element.description;
     /*  создаем ссылку А класса  gallery__link*/
     const link = document.createElement("a");
     link.classList.add("gallery__link");
-    link.href = element.preview;
+    link.href = element.original;
     link.appendChild(img);
     /* создаем єлемент li    */
     const list = document.createElement("li");
     list.classList.add("gallery__item");
     console.log(list);
     list.appendChild(link);
+    refs.gallery.append(list);
   });
 elementGalleryRef(array);
-
-/*refs.gallery.addEventListener("click", onGalleryClick);
+/* кликаем на галлерею*/
+refs.gallery.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(event) {
   event.preventDefault();
@@ -36,14 +38,17 @@ function onGalleryClick(event) {
   if (event.target.nodeName !== "IMG") {
     return;
   }
-
   const imageRef = event.target;
   const largeImageURL = imageRef.dataset.source;
-  console.log(imageRef);
   console.log(largeImageURL);
   setLargeImageSrc(largeImageURL);
-}
 
+  /*обавляем класс на div[class=lightbox*/
+  const modalOn = refs.modalInput;
+  modalOn.classList.add("is-open");
+  console.log(refs.modalInput);
+}
 function setLargeImageSrc(url) {
-  refs.largeImage.src = url;
-}*/
+  console.dir(refs.target);
+  refs.gallery__image.src = url;
+}
